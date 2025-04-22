4f3ff40a1cef43724f31b7866fa7e2ae6fd17a7d
jeifferm1217 <jeiffermosquera@gmail.com>
2025-04-21 21:52:39 -0500

    solucion de ejercicios finales

---
+++ Fundamentos del Lenguaje/Ejercicios Finales/conversiones.py
@@ -0,0 +1,13 @@
+
+
+def cm_a_pulgadas(cm):
+    return cm / 2.54
+
+def pulgadas_a_cm(pulgadas):
+    return pulgadas * 2.54
+
+def metros_a_pies(metros):
+    return metros * 3.281
+
+def pies_a_metros(pies):
+    return pies / 3.281
